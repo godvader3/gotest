0a2b267606546f8ba50b9756aa18a340ab841477
Mike <god_vader3@yahoo.com>
2021-05-14 03:50:32 +0000

    test tag1 dev

--- main.go
+++ main.go
@@ -7,7 +7,7 @@ import (
 
 func main() {
     http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-        fmt.Fprintf(w, "Hello New World112233")
+        fmt.Fprintf(w, "Hello New World11 Dev")
     })
 
     http.HandleFunc("/greet/", func(w http.ResponseWriter, r *http.Request) {
